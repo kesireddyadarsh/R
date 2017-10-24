4444eea1e559cb4fbd4f5f0e2f20876849bea5dc
kesireddyadarsh <akesireddy@nevada.unr.edu>
2017-10-24 00:53:20 -0700

    Add files via upload

--- midterm.R
+++ midterm.R
@@ -48,3 +48,71 @@ hyperexpo <- function(number_of_samples,p_1,p_2,lambda_1,lambda_2){
 
 value <-hyperexpo(5000,0.3,0.7,1,0.1)
 hist(value)
+
+#problem 4
+MH_norm <- function(n, alpha,Y) 
+{
+  samples <- vector("numeric", n)
+  x <- 0
+  #x <- 10  # What happens if your initial value is far from the true distribution?
+  samples[1] <- x
+  for (i in 2:n) {
+    innov <- Y
+    proposal <- x + innov
+    aprob <- min(1, dnorm(proposal,0,1)/dnorm(x,0,1))
+    u <- runif(1)
+    if (u < aprob){
+      x <- proposal
+    } 
+    samples[i] <- x
+  }
+  samples
+}
+
+
+# Run the MH function above
+run1 <- MH_norm(1000,1,value)
+
+# Plot samples as a time series (partition the plot window into 2 panels)
+par(mfrow=c(2,1))
+plot(ts(run1), xlab="Iteration", ylab="Sampled Values", col="darkgreen",
+     main="Metropolis-Hastings MCMC: Normal Target Distribution")
+
+# Plot a histogram of sampled values
+hist(run1, breaks=30, freq=FALSE, col="wheat", main="Histogram of Sampled Values", xlab="Sampled Values")
+par(mfrow=c(1,1))
+
+
+#Trying out
+MH_norm <- function(n, alpha) 
+{
+  samples <- vector("numeric", n)
+  x <- 0
+  #x <- 10  # What happens if your initial value is far from the true distribution?
+  samples[1] <- x
+  for (i in 2:n) {
+    innov <- runif(1, -alpha, alpha)
+    proposal <- x + innov
+    aprob <- min(1, dnorm(proposal,0,1)/dnorm(x,0,1))
+    u <- runif(1)
+    if (u < aprob){
+      x <- proposal
+    } 
+    samples[i] <- x
+  }
+  samples
+}
+
+
+# Run the MH function above
+run1 <- MH_norm(1000,1)
+
+# Plot samples as a time series (partition the plot window into 2 panels)
+par(mfrow=c(2,1))
+par(mar = c(1,1))
+plot(ts(run1), xlab="Iteration", ylab="Sampled Values", col="darkgreen",
+     main="Metropolis-Hastings MCMC: Normal Target Distribution")
+
+# Plot a histogram of sampled values
+hist(run1, breaks=30, freq=FALSE, col="wheat", main="Histogram of Sampled Values", xlab="Sampled Values")
+par(mfrow=c(1,1))
